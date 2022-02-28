f71d4d5b18b1adcf9024a3ad39202f6699f2624e
dstoffels <dan.stoffels@gmail.com>
2022-02-28 13:45:36 -0600

    adjusted VideoPage folder hierarchy

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import apiPaths from './Constants/apiPaths';
 import NavBar from './Components/NavBar/NavBar/NavBar.jsx';
 import './App.css';
 import SignInModal from './Components/SignInModal/SignInModal/SignInModal.jsx';
@@ -7,11 +6,10 @@ import { useDispatch } from 'react-redux';
 import { setUser } from './Redux/userSlice.js';
 import jwtDecode from 'jwt-decode';
 import SignUpModal from './Components/SignUpModal/SignUpModal.jsx';
-import VideoPlayer from './Components/VideoPlayer/VideoPlayer';
 import { Routes, Route } from 'react-router-dom';
 import Home from './Components/Home/Home';
 import SearchResults from './Components/SearchResults/SearchResults';
-import VideoPage from './Components/VideoPage/VideoPage';
+import VideoPage from './Components/VideoPage/VideoPage/VideoPage.jsx';
 
 function App() {
 	const dispatch = useDispatch();
@@ -28,11 +26,11 @@ function App() {
 			<SignInModal />
 			<SignUpModal />
 			<NavBar />
-      <Routes>
-        <Route exact path ="/" element={<Home/>}/>
-        <Route path ="search" element={<SearchResults/>}/>
-        <Route path ="video" element={<VideoPage/>}/>
-      </Routes>
+			<Routes>
+				<Route exact path='/' element={<Home />} />
+				<Route path='search' element={<SearchResults />} />
+				<Route path='video' element={<VideoPage />} />
+			</Routes>
 		</div>
 	);
 }
---
+++ src/Components/VideoPage/VideoPage/VideoPage.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import VideoPlayer from '../VideoPlayer/VideoPlayer';
+
+const VideoPage = props => {
+	return <VideoPlayer />;
+};
+
+export default VideoPage;
