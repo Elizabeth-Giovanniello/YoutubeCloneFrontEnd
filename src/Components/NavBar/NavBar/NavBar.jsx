fd532fe87b4e84372807704dedcae101d63fda57
dstoffels <dan.stoffels@gmail.com>
2022-03-01 10:04:33 -0600

    updated navigation in home btn

--- src/Components/NavBar/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar/NavBar.jsx
@@ -6,16 +6,22 @@ import SignInButton from '../../Common/SignInButton/SignInButton';
 import { useSelector } from 'react-redux';
 import LogoutButton from '../LogOutBtn/LogOutBtn.jsx';
 import SearchBar from '../SearchBar/SearchBar.jsx';
+import { useNavigate } from 'react-router-dom';
+import { HOME } from '../../../Constants/routes.js';
 
 const NavBar = props => {
 	const currentUser = useSelector(state => state.user);
+	const navigate = useNavigate();
 
 	const userDisplay = currentUser.user_id ? <LogoutButton /> : <SignInButton />;
 
 	return (
 		<Navbar sticky='top' bg='dark' variant='dark'>
 			<Container fluid className='text-white'>
-				<Navbar.Brand>
+				<Navbar.Brand
+					onClick={() => {
+						navigate(HOME);
+					}}>
 					<Film size={35} />
 					{' WeTube'}
 				</Navbar.Brand>
