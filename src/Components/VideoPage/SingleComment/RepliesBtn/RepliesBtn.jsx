52e0071e97fe6fa62d8753a3446422a7d45efd1c
dstoffels <dan.stoffels@gmail.com>
2022-03-02 11:54:30 -0600

    bugfix: made replies button text unselectable

--- src/Components/VideoPage/SingleComment/RepliesBtn/RepliesBtn.jsx
+++ src/Components/VideoPage/SingleComment/RepliesBtn/RepliesBtn.jsx
@@ -1,12 +1,13 @@
-import { CaretDownFill, CaretUpFill } from 'react-bootstrap-icons';
 import React from 'react';
+import { CaretDownFill, CaretUpFill } from 'react-bootstrap-icons';
+import './RepliesBtn.css';
 
 const RepliesBtn = ({ open, onClick }) => {
 	const handleClick = () => {
 		onClick(!open);
 	};
 	return (
-		<div className='text-primary ps-0 clickable' onClick={handleClick}>
+		<div className='text-primary ps-0 clickable replies-btn' onClick={handleClick}>
 			{open ? <CaretUpFill /> : <CaretDownFill />}
 			{open ? ' Hide' : ' View'} replies
 		</div>
