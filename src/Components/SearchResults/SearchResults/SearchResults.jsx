afaad6b10bf1e81c847df10dd97c59df2f7a18b0
dstoffels <dan.stoffels@gmail.com>
2022-03-02 17:50:33 -0600

    updated searchReults page to navigate to home if there is no search term

--- src/Components/SearchResults/SearchResults/SearchResults.jsx
+++ src/Components/SearchResults/SearchResults/SearchResults.jsx
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Container } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import { HOME } from '../../../Constants/routes.js';
 import SearchVideoCard from '../VideoCardSearch/VideoCardSearch.jsx';
 
 const SearchResults = props => {
+	// HOOKS
 	const navigate = useNavigate();
-	const searchResults = useSelector(state => state.search.results);
+
+	// STATE
+	const { term, results } = useSelector(state => state.search);
+
+	useEffect(() => {
+		if (!term) navigate(HOME);
+	}, []);
 
 	return (
 		<Container>
-			{searchResults.map(function (video) {
+			{results.map(function (video) {
 				return <SearchVideoCard key={video.id.videoId} video={video} />;
 			})}
 		</Container>
