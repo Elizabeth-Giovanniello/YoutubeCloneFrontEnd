425e79d8c17404bde58e6ad0ac365b0f13e198eb
Lizzy <ea.giovanniello@gmail.com>
2022-03-02 10:47:36 -0500

    Updating API paths.

--- src/Constants/apiPaths.js
+++ src/Constants/apiPaths.js
@@ -1,14 +1,14 @@
 const root = 'http://127.0.0.1:8000';
 
-export const addCommentPath = root + '/comments/',
-	addReplyPath = root + '/replies/',
+export const addCommentPath = root + '/add_comment/',
+	addReplyPath = root + '/add_reply/',
 	loginPath = root + '/auth/login/',
 	tokenRefreshPath = root + '/auth/login/refresh/',
 	registerPath = root + '/auth/register/';
 
 export const allCommentsPath = videoID => `${root}/${videoID}/`;
-export const allRepliesPath = commentID => `${addCommentPath}${commentID}/`;
-export const editCommentPath = commentID => `${root}/comments/${commentID}/`;
-export const editReplyPath = replyID => `${root}/${replyID}/`;
-export const toggleLikePath = (type, responseID) => `${root}/${type}/${responseID}/like/`;
-export const toggleDislikePath = (type, responseID) => `${root}/${type}/${responseID}/dislike/`;
+export const allRepliesPath = commentID => `view_replies/${commentID}/`;
+export const editCommentPath = commentID => `${root}/edit_comment/${commentID}/`;
+export const editReplyPath = replyID => `${root}/edit_reply/${replyID}/`;
+export const toggleLikePath = (type, responseID) => `${root}/like/${type}/${responseID}/`;
+export const toggleDislikePath = (type, responseID) => `${root}/dislike/${type}/${responseID}/`;
